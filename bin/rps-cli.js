37c909ca19f9727d3e7cdea678e0683965116197
StevenAMartin <samart@email.unc.edu>
2023-02-26 23:33:46 -0400

    Tweaks

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -4,17 +4,9 @@ import { rps } from "../lib/rpsls.js"
 
 const args = minimist(process.argv.slice(2));
 
-if (args.h || args.help) {
-    help();
-    process.exit(0);
-}
+if (args.h || args.help) { help(); }
 
-if (args.r || args.rules) {
-    rules();
-    process.exit(0);    
-}
-
-let game = undefined;
+if (args.r || args.rules) { rules(); }
 
 try {
     console.log(JSON.stringify(rps(args._[0])));
@@ -26,22 +18,22 @@ try {
     }
 }
 
-
   
 
 function help() {
     console.log(
-        `Usage: node-rps [SHOT]
-        Play Rock Paper Scissors (RPS)
-        
-            -h, --help      display this help message and exit
-            -r, --rules     display the rules and exit
-        
-        Examples:
-            node-rps        Return JSON with single player RPS result.
-                            e.g. {"player":"rock"}
-            node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                            e.g {"player":"rock","opponent":"scissors","result":"win"}`);    
+`Usage: node-rps [SHOT]
+Play Rock Paper Scissors (RPS)
+
+    -h, --help      display this help message and exit
+    -r, --rules     display the rules and exit
+
+Examples:
+    node-rps        Return JSON with single player RPS result.
+                    e.g. {"player":"rock"}
+    node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"scissors","result":"win"}`);    
+    process.exit(0);    
 }
 
 function rules() {
@@ -51,4 +43,5 @@ function rules() {
 - Scissors CUTS Paper
 - Paper COVERS Rock
 - Rock CRUSHES Scissors`);
+    process.exit(0);    
 }
